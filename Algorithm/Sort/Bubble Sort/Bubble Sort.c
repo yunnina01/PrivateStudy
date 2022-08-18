973d71259b72b36356ded61def3d5aec1508515a
yunnina01 <yunnina01@naver.com>
2022-08-18 14:33:54 +0900

    Code fixing

--- Algorithm/Sort/Bubble Sort/Bubble Sort.c
+++ Algorithm/Sort/Bubble Sort/Bubble Sort.c
@@ -3,7 +3,7 @@
 
 void bubble_sort(int *arr, int size){
     for(int i=0; i<size-1; i++){
-        for(int j=0; j<size-1; j++){
+        for(int j=0; j<size-i-1; j++){
             if(arr[j] > arr[j+1]){
                 int temp = arr[j];
                 arr[j] = arr[j+1];
