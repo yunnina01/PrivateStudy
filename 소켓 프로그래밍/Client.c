d242104e53ef6b6ac86e654eae275e5c2a5554d8
TaeHyun Yun <yunnina01@naver.com>
2024-04-16 22:30:28 +0900

    주석 추가

--- 소켓 프로그래밍/Client.c
+++ 소켓 프로그래밍/Client.c
@@ -1,16 +1,19 @@
+// 소켓 통신의 클라이언트 부분 구현
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <winsock2.h>
 
-void errorHandling(char *msg){
+// 에러 핸들링
+void errorHandling(char *msg) {
     WSACleanup();
     fputs(msg, stderr);
     fputc('\n', stderr);
     exit(1);
 }
 
-void socketConnection(SOCKET *clientSock, SOCKADDR_IN servAddr, char *host, int port){
+// 소켓 연결
+void socketConnection(SOCKET *clientSock, SOCKADDR_IN servAddr, char *host, int port) {
 	*clientSock = socket(PF_INET, SOCK_STREAM, 0);
 	if(*clientSock == INVALID_SOCKET)
 		errorHandling("socket() error!");
@@ -24,7 +27,7 @@ void socketConnection(SOCKET *clientSock, SOCKADDR_IN servAddr, char *host, int 
 		errorHandling("connect() error!");
 }
 
-int main(int argc, char *argv[]){
+int main(int argc, char *argv[]) {
     WSADATA wsaData;
 	SOCKET clientSock;
 	SOCKADDR_IN servAddr;
@@ -39,15 +42,18 @@ int main(int argc, char *argv[]){
 	
 	socketConnection(&clientSock, servAddr, host, port);
 
+	// 서버로 메시지 전송
 	printf("Send to Server : ");
 	scanf("%s", message);
 	send(clientSock, message, strlen(message) + 1, 0);
 
+	// 서버로부터 메시지 수신
     strLen = recv(clientSock, message, sizeof(message) - 1, 0);
 	if(strLen <= 0)
 		errorHandling("read() error!");
     printf("Receive from Server : %s\n", message);
-	
+
+	// 소켓 연결 해제
     closesocket(clientSock);
 	WSACleanup();
 
--- 소켓 프로그래밍/Server.c
+++ 소켓 프로그래밍/Server.c
@@ -1,15 +1,18 @@
+// 소켓 통신의 서버 부분 구현
 #include <stdio.h>
 #include <stdlib.h>
 #include <winsock2.h>
 
-void errorHandling(char *msg){
+// 에러 핸들링
+void errorHandling(char *msg) {
     WSACleanup();
     fputs(msg, stderr);
     fputc('\n', stderr);
     exit(1);
 }
 
-void socketBinding(SOCKET *servSock, SOCKET *clientSock, SOCKADDR_IN servAddr, SOCKADDR_IN clientAddr, char *host, int port){
+// 소켓 바인딩
+void socketBinding(SOCKET *servSock, SOCKET *clientSock, SOCKADDR_IN servAddr, SOCKADDR_IN clientAddr, char *host, int port) {
     *servSock = socket(PF_INET, SOCK_STREAM, 0);
 	if(*servSock == INVALID_SOCKET)
 		errorHandling("socket() error!");
@@ -30,13 +33,13 @@ void socketBinding(SOCKET *servSock, SOCKET *clientSock, SOCKADDR_IN servAddr, S
     *clientSock = accept(*servSock, (void*)&clientAddr, &fromLen);
     if(*clientSock == INVALID_SOCKET)
         errorHandling("accept() error!");
-    else{
+    else {
         printf("%s Connection Complete\n", inet_ntoa(clientAddr.sin_addr));
         printf("Start ...\n");
     }
 }
 
-int main(){
+int main() {
     WSADATA wsaData;
 	SOCKET servSock, clientSock;
 	SOCKADDR_IN servAddr, clientAddr;
@@ -51,18 +54,20 @@ int main(){
 
     socketBinding(&servSock, &clientSock, servAddr, clientAddr, host, port);
 
+    // 클라이언트로부터 메시지 수신
     nRcv = recv(clientSock, message, sizeof(message) - 1, 0);
     if(nRcv == SOCKET_ERROR)
         errorHandling("recv() error!");
     message[nRcv] = '\0';
     printf("Receive from Client : %s\n", message);
 
+    // 클라이언트로 메시지 전송
 	printf("Send to Client : ");
 	scanf("%s", message);
 	send(clientSock, message, strlen(message) + 1, 0);
 
+    // 소켓 연결 해제
     closesocket(servSock);
-
 	WSACleanup();
 
     return 0;
